b742f04420de13c7c7fb9e1b3f8b555a9bc4ac94
Shruti Rastogi <shruti.rastogi2501@gmail.com>
2021-08-04 22:09:55 +0530

    Functions

--- functions.ts
+++ functions.ts
@@ -2,17 +2,21 @@
 //     console.log(n1 + n2)
 //     return n1 + n2;
 // }
+
 // function printResult(num: number): void {
 //     console.log('Result :' + num);
 // }
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
+
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+    const result = n1 + n2;
+    cb(result)
 }
-addAndHandle(10, 20, function (result) {
-    console.log(result);
+addAndHandle(10, 20, (result) => {
+    console.log(result)
 });
+
 // printResult(add(1, 2));
+
 // let combineValues: (a: number, b: number) => number;
 // combineValues = add;
 // combineValues = printResult;
